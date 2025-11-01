b951e7c6a85ac792f41285cebe140a0c32ba5dd3
Ahmed <ahmedelmansy579@gmail.com>
2025-11-01 14:40:07 +0200

    test ahmed

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,8 @@
 export default function Shop() {
-    return (
-        <div>
-            <h1>Shop page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Shop page</h1>
+      <h1> Test for Ahmed </h1>
+    </div>
+  );
 }
