7dfe73d854612613a092e518a09adf45b2ba4692
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-11-01 22:38:18 +0200

    fixed breadcrumbs to be shown in other pages but not home

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,20 +1,21 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 export default function Breadcrumbs() {
     const location = useLocation();
     
     const getPageName = () => {
         const path = location.pathname.replace('/', '');
-        if (!path) return 'Home';
         return path.replace('-', ' ');
     };
 
     const currentPage = getPageName();
     
+    if (location.pathname === '/') return null;
+    
     return (
         <div className="bg-white">
             <div className="max-w-7xl mx-auto flex items-center gap-2 sm:gap-3 text-[13px] sm:text-sm text-gray-500 py-3 px-4 uppercase">
-                <span className="text-[#3E445A] cursor-pointer transition-colors">Home</span>
+                <Link to="/" className="text-[#3E445A] cursor-pointer transition-colors !hover:text-gray-700">Home</Link>
                 <i className="fa-solid fa-chevron-right text-[10px] text-gray-400"></i>
                 <span className="text-gray-500">{currentPage}</span>
             </div>
